bfa00ec4be6e7e16e9691e114e13cb6f8295e481
nurinsyahminaa02 <nurinsyahminaa02@gmail.com>
2026-06-24 15:26:59 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -555,7 +555,7 @@ if img_base64:
         <img src="data:image/png;base64,{img_base64}" width="60">
         <h2 style="
             margin: 0;
-            color: #e0f2f1;
+            color: #020302;
             font-family: 'Arial', sans-serif;
             font-weight: bold;
             font-size: 22px;
