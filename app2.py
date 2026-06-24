98eee4123d87ea76554ee4b8d85e28459af51b25
nurinsyahminaa02 <nurinsyahminaa02@gmail.com>
2026-06-24 15:46:22 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageDraw
 st.set_page_config(
     page_title="PERBAH",
     page_icon="TDM.png",
-    layout="mindle",
+    layout="centered",
     initial_sidebar_state="expanded"
 )
 
